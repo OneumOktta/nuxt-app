a94e1572a8cbaf2a649ee78363abd4859a5f6474
Roman Ulitin <kurt-77@mail.ru>
2024-12-12 10:51:36 +0500

    test

--- app/layouts/panel.vue
+++ app/layouts/panel.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
-import PanelHeader from '~/components/panel/PanelHeader.vue'
-import PanelSidebar from '~/components/panel/PanelSidebar.vue'
+import PanelHeader from "~/components/panel/PanelHeader.vue";
+import PanelSidebar from "~/components/panel/PanelSidebar.vue";
 </script>
 
 <template>
-	<div class="min-h-screen flex flex-col">
-		<PanelHeader />
-		<div class="flex flex-1">
-			<PanelSidebar />
-			<main class="flex-1 pl-2 pr-6 pb-6 pt-2">
-				<slot />
-			</main>
-		</div>
-	</div>
+  <div class="min-h-screen flex flex-col bg-gray-50">
+    <PanelHeader />
+    <div class="flex flex-1">
+      <PanelSidebar />
+      <main class="flex-1 pl-2 pr-6 pb-6 pt-2">
+        <slot />
+      </main>
+    </div>
+  </div>
 </template>
